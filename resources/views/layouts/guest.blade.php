2327af070ab0a610cd34e907152a5955b1e4150d
andiii24 <andiialpha@gmail.com>
2026-01-05 16:59:16 -0500

    Revert "vite issue fixed"
    This reverts commit 131c5671cfe11746ad2ebd9f9ae5806e34c78914.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,15 +19,7 @@
             <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
             <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
         @else
-                    @if(file_exists(public_path('build/manifest.json')))
-            @php
-                $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-            @endphp
-            <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
-            <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
-        @else
             @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
         @endif
     </head>
     <body class="font-sans text-gray-900 antialiased">
